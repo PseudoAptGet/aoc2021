8207e6fd0143cab1680a786883bf01af059e46b8
PseudoAptGet <ananabrar02@gmail.com>
2022-01-04 17:47:25 -0500

    day 2 challenge

---
+++ TestingShit.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class TestingShit {
+
+
+
+    public static void main(String[] args){
+        try {
+            File file = new File("directions.txt");
+            Scanner sc = new Scanner(file);
+            int height = 0;
+            int distance = 0; 
+            int aim = 0; 
+            while(sc.hasNext()){
+                String[] curVal = sc.nextLine().split(" ");
+                String value = (String)curVal[0];
+                switch (value){
+                    case "up": aim -= Integer.parseInt((String)curVal[1]); break;
+                    case "down": aim += Integer.parseInt((String)curVal[1]); break;
+                    case "forward": distance += Integer.parseInt((String)curVal[1]); 
+                        height += Integer.parseInt((String)curVal[1]) * aim;
+                        break;
+                }
+            }
+            System.out.println(height);
+            System.out.println(distance);
+            System.out.println(Math.abs(height * distance));
+            sc.close();
+        }
+        catch(Exception FileNotFoundException) {
+            System.out.println("shit");
+        }
+        
+    }
+}
